d475b75a592ed2c4c009edd34812439674eb8bc3
Masum Billal <mbillal.eee@gmail.com>
2023-05-23 21:53:47 +0600

    7 : Middleware in Express

--- server/src/server.js
+++ server/src/server.js
@@ -3,28 +3,38 @@ const morgan = require("morgan");
 
 const app = express();
 
-app.use(morgan("dev"));  //
+app.use(morgan('dev'));
+app.use(express.json());  //json data
+app.use(express.urlencoded({extended: true}));  //form releted data build in method
 
-app.get("/test", (request, response)=>{
+const isLoggedIn = (request, response, next) =>{
+    console.log("isLoggedIn Middleware");
+    const login = true;
+    if(login){
+        request.body.id = 101;
+        next();
+    }else{
+        return response.status(404).json({
+            message: 'Please login first'
+        });
+    }
+    next();
+}
+// app.use(isLoggedIn);
+
+app.get('/test', (request, response)=>{
     response.status(200).send({
         message: "API testing is testing fine",
     });
 });
-app.post("/test", (request, response)=>{
+app.get('/api/user',isLoggedIn, (request, response)=>{
+    console.log(request.body.id);
+    // console.log('user Profile');
     response.status(200).send({
-        message: "post: API testing is testing fine",
+        message: "User profile is returm",
     });
 });
-app.put("/test", (request, response)=>{
-    response.status(200).send({
-        message: "put: API testing is testing fine",
-    });
-});
-app.delete("/test", (request, response)=>{
-    response.status(200).send({
-        message: "delete: API testing is testing fine",
-    });
-});
+
 
 app.listen(3001, ()=>{
     console.log(`Server is running at http://localhost:3001`);
