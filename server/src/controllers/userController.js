3bd25ffed86d0704ae64aca16e88a8f753dfba64
Masum Billal <mbillal.eee@gmail.com>
2023-05-24 02:23:14 +0600

    14 : User schema and model

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,5 +1,4 @@
 const createError = require('http-errors');
-const users = require('../models/userModel');
 
 
 
--- server/src/models/userModel.js
+++ server/src/models/userModel.js
@@ -1,7 +1,58 @@
-const users = [
-    {id:1, name: 'Masum'},
-    {id:2, name: 'Billal'},
-    {id:3, name: 'Mukta'},
-]
+const {Schema, model} = require("mongoose");
+const bcrypt = require('bcrypt');
+const { defaultImagePath } = require("../secret");
 
-module.exports = users;
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'User name is required'],
+        trim: true,
+        minlength: [3, "User name can be minimum 3 characters"],
+        maxlength: [31, "User name can be maximum 32 characters"]
+    },
+    email: {
+        type: String,
+        required: [true, 'User email is required'],
+        trim: true,
+        unique: true,
+        lowercase:true,
+        validate: {
+            validator: function(v){
+                return /^w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+            },
+            message: 'Please enter a valid email'
+        }
+    },
+    password: {
+        type: String,
+        required: [true, 'User password is required'],
+        trim: true,
+        minlength: [6, "User password can be minimum 6 characters"],
+        set: (v) =>bcrypt.hashSync(v, bcrypt.genSaltSync(10))
+    },
+    image: {
+        type: String,
+        default: defaultImagePath,
+    },
+    address: {
+        type: String,
+        required: [true, 'User address is required'],
+
+    },
+    phone: {
+        type: String,
+        required: [true, 'User phone is required'],
+
+    },
+    isBan:{
+        type: Boolean,
+        default: false
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    }, 
+}, {timestamps:true});
+
+const User = model('Users', userSchema);
+module.exports = User;
